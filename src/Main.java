7d2cbceb3199173512a5192c8a13d4b96e142e75
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-26 23:47:20 +0300

    tests added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Test code for Point class
+        Point point1 = new Point(6, 5);
+        Point point2 = new Point(3, 1);
+        Point point = new Point(0, 0);
+        System.out.println("distance(0,0)= " + point1.distance());
+        System.out.println("distance(second)= " + point1.distance(point2));
+        System.out.println("distance(2,2)= " + point1.distance(2, 2));
+        System.out.println("distance()= " + point.distance());
     }
 }
